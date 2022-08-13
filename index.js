96d8506a459bb313865cdfd1e502692bde5f5b32
Chan Lee <clee09818@gmail.com>
2022-08-12 23:50:01 -0400

    commit tree d3491b9babc2826dd9e382ce1e027283d08f0875

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+
+
+ 
+
+  function writeCards(name, event){
+    const newArray = [];
+    for(let i = 0; i < name.length; i++){
+        newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArray;
+  }
+
+
+  function countDown(){
+    let i = 10
+    while(i <= 10 && i >= 0 ){
+      console.log(i);
+      i--
+    }
+
+  }
